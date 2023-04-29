a51fdc5ea16f9ce03d4644be5a0359f1a43fb500
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-04-29 17:56:22 +0530

    building chat app

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,4 @@
-import { useEffect } from 'react';
-import { getPosts } from '../api';
-
 function App() {
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await getPosts();
-      console.log('response', response);
-    };
-
-    fetchPosts();
-  }, []);
-
   return (
     <div className="App">
       <h1>Hello world</h1>
