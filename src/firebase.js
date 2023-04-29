3b0af2054df39da51d914090ab5c2a94413d3419
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-04-29 18:20:42 +0530

    setting up firebase

---
+++ src/firebase.js
@@ -0,0 +1,27 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBtULNASJfE4ZJCBqXRrWitItqdisgTUEc",
+    authDomain: "chat-app-55e2b.firebaseapp.com",
+    projectId: "chat-app-55e2b",
+    storageBucket: "chat-app-55e2b.appspot.com",
+    messagingSenderId: "524518056018",
+    appId: "1:524518056018:web:af0a2441970b0600acff8b"
+  };
+  
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+export const firestore = firebase.firestore();
