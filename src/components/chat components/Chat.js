6285fa223390bd7696595d70d6ceebe38c5e90fa
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-05-01 19:44:23 +0530

    add style to chatbox

--- src/components/chat components/Chat.js
+++ src/components/chat components/Chat.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import styles from '../../styles/chat.module.css'
 
 const Chat = () => {
   return (
-    <div>Chat</div>
+    <div className={styles.container}>
+        <div className={styles.chatbox}></div>
+        <div className={styles.inputbox}>
+            <input placeholder='type here' />
+            <input type='file' />
+            <button>Send</button>
+        </div>
+    </div>
   )
 }
 
