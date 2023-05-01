0e81b1fb10a1c9e49afd70fc2980594309fe3ab1
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-05-01 20:30:03 +0530

    add style to chat

--- src/components/chat components/Chat.js
+++ src/components/chat components/Chat.js
@@ -7,8 +7,13 @@ const Chat = () => {
         <div className={styles.chatbox}></div>
         <div className={styles.inputbox}>
             <input placeholder='type here' />
-            <input type='file' />
-            <button>Send</button>
+            <div className={styles.send}>
+                <input type='file' style={{display:"none"}} id='file' />
+                <label htmlFor='file'>
+                    <img src='https://cdn-icons-png.flaticon.com/512/1246/1246111.png?w=740&t=st=1682951888~exp=1682952488~hmac=25c6c8fb7bd06da0b952cbf6ffe39df6e281ab8ebea4a0803169512bc2f4276e' alt='attach' />
+                </label>
+                <button>Send</button>
+            </div>
         </div>
     </div>
   )
