c1a4b83d444b0cbbc1e887187a8c64952f7b3081
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-05-01 19:25:07 +0530

    aside styling done

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,13 @@ import Profile from './chat components/Profile'
 import ChatNavbar from './chat components/AsideNavbar'
 import styles from '../styles/home.module.css'
 import Aside from './chat components/Aside'
+import Chatbox from './chat components/Chatbox'
 
 const Home = () => {
   return (
     <div className={styles.container}>
       <Aside />
+      <Chatbox />
     </div>
   )
 }
--- src/components/chat components/Aside.js
+++ src/components/chat components/Aside.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import AsideNavbar from './AsideNavbar'
 import FriendsList from './FriendsList'
+import styles from '../../styles/aside.module.css'
 
 const Aside = () => {
   return (
-    <div>
+    <div className={styles.container}>
         <AsideNavbar />
         <FriendsList />
     </div>
