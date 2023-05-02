3d6afdb89442542d7e0b3e53d63ba4ee438de817
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-05-02 22:11:39 +0530

    chat context and chat reducer created

--- src/auth/ChatContext.js
+++ src/auth/ChatContext.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import { createContext, useContext, useReducer,} from "react";
+import { AuthContext } from "./AuthContext";
+  
+export const ChatContext = createContext();
 
-const ChatContext = () => {
-  return (
-    <div>ChatContext</div>
-  )
-}
+export const ChatContextProvider = ({ children }) => {
+const { currentUser } = useContext(AuthContext);
+const INITIAL_STATE = {
+    chatId: "null",
+    user: {},
+};
 
-export default ChatContext
+const chatReducer = (state, action) => {
+    switch (action.type) {
+    case "CHANGE_USER":
+        return {
+        user: action.payload,
+        chatId:
+            currentUser.uid > action.payload.uid
+            ? currentUser.uid + action.payload.uid
+            : action.payload.uid + currentUser.uid,
+        };
+
+    default:
+        return state;
+    }
+};
+
+const [state, dispatch] = useReducer(chatReducer, INITIAL_STATE);
+
+return (
+    <ChatContext.Provider value={{ data:state, dispatch }}>
+    {children}
+    </ChatContext.Provider>
+);
+};
