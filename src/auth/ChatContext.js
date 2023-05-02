5ea23ff2248819fd98b0c82224012a4bef2d8276
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-05-02 22:09:41 +0530

    create friend list

---
+++ src/auth/ChatContext.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChatContext = () => {
+  return (
+    <div>ChatContext</div>
+  )
+}
+
+export default ChatContext
--- src/components/chat components/FriendsList.js
+++ src/components/chat components/FriendsList.js
@@ -1,36 +1,49 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import {AuthContext} from "../../auth/AuthContext";
+import {ChatContext} from "../../auth/ChatContext";
+import { db } from "../../firebase";
 
 const FriendsList = () => {
-    useEffect(() => {
-      
-    }, [])
-    
+    const [chats, setChats] = useState([]);
 
+  const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+
+      return () => {
+        unsub();
+      };
+    };
+
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+
+  const handleSelect = (u) => {
+    dispatch({ type: "CHANGE_USER", payload: u });
+  };
   return (
-    <div className='userChats'>
-      <div className='userChat'>
-            <img src='https://i.pinimg.com/736x/50/53/87/505387eec410eb22c31006fbb5255279.jpg' alt='user' />
-            <div className='userChatInfo'>
-                <span>Jane</span>
-                <p>Hello</p>
-            </div>
+    <div className="userChats">
+      {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
+        <div
+          className="userChat"
+          key={chat[0]}
+          onClick={() => handleSelect(chat[1].userInfo)}
+        >
+          <img src={chat[1].userInfo.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{chat[1].userInfo.displayName}</span>
+            <p>{chat[1].lastMessage?.text}</p>
+          </div>
         </div>
-        <div className='userChat'>
-            <img src='https://i.pinimg.com/736x/50/53/87/505387eec410eb22c31006fbb5255279.jpg' alt='user' />
-            <div className='userChatInfo'>
-                <span>Jane</span>
-                <p>Hello</p>
-            </div>
-        </div>
-        <div className='userChat'>
-            <img src='https://i.pinimg.com/736x/50/53/87/505387eec410eb22c31006fbb5255279.jpg' alt='user' />
-            <div className='userChatInfo'>
-                <span>Jane</span>
-                <p>Hello</p>
-            </div>
-        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default FriendsList
